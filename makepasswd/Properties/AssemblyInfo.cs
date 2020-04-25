988ddb49e4fcb85e27ff5e63f2acf8f9d0deb0a5
mayrand <mayrand@users.noreply.github.com>
2020-04-25 11:04:17 +0200

    makepasswd
    Generate password and its md5 using linux like implementation

---
+++ makepasswd/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("makepasswd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trader.com (Polska) Sp. z o.o")]
+[assembly: AssemblyProduct("makepasswd")]
+[assembly: AssemblyCopyright("Copyright © Trader.com (Polska) Sp. z o.o 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ac4941f-539b-43eb-b889-c4c15247267c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ makepasswd/makepasswd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TraderUtils
+{
+	public class makepasswd
+	{
+		public readonly string password;
+		public readonly string md5;
+		public makepasswd()
+		{
+			password = RandomPassword.Generate(10);
+			md5 = MD5Crypt.crypt(password, string.Empty);
+		}
+	}
+}
